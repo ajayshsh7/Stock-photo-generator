29eb5f9afeffd6b5e5e86c561f01f18384f0f6ef
ajayshsh7 <ajayshsh7@gmail.com>
2025-06-22 16:31:53 +0530

    css fixes

--- app.js
+++ app.js
@@ -78,3 +78,18 @@ async function loadMore() {
 }
 
 curatedPhotos();
+
+const track = document.querySelector('.carousel-track');
+    const slides = Array.from(track.children);
+    let currentIndex = 0;
+
+    function moveToSlide(index) {
+      track.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    function nextSlide() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      moveToSlide(currentIndex);
+    }
+
+    setInterval(nextSlide, 3000);
